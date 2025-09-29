5d509755a05b4ce7c690ee55a8b446b3e758daf5
J-Dymond <jackdymond@me.com>
2025-09-29 13:13:57 +0100

    added some utilities for processing callhome dataset

--- src/dr_sad/callhome_utils.py
+++ src/dr_sad/callhome_utils.py
@@ -1,3 +1,5 @@
+import itertools
+
 
 def roundrobin(*iterables: list) -> any:
     """
@@ -17,6 +19,7 @@ def roundrobin(*iterables: list) -> any:
         except StopIteration:
             iterators.remove(it)
 
+
 def add_gaps(old_sample: dict) -> dict:
     """
     Adds gaps in the audio sample where there is no speech.
@@ -27,21 +30,34 @@ def add_gaps(old_sample: dict) -> dict:
     Returns:
         The modified audio sample with gaps added.
     """
-    sample = old_sample.copy()
-    talking_periods = [(start, end, speaker) for start, end, speaker in zip(sample['timestamps_start'], sample['timestamps_end'], sample['speakers'], strict=True)]
-    non_talking_periods = [(start,end,"None") for (_, start,_), (end,_,_) in zip(talking_periods[:-1], talking_periods[1:], strict=True)]
-    audio_duration = len(sample['audio']['array']) / sample['audio']['sampling_rate']
+    new_sample = old_sample.copy()
+    talking_periods = [
+        (start, end, speaker)
+        for start, end, speaker in zip(
+            new_sample["timestamps_start"],
+            new_sample["timestamps_end"],
+            new_sample["speakers"],
+            strict=True,
+        )
+    ]
+    non_talking_periods = [
+        (start, end, "None")
+        for (_, start, _), (end, _, _) in itertools.pairwise(talking_periods)
+    ]
+    audio_duration = (
+        len(new_sample["audio"]["array"]) / new_sample["audio"]["sampling_rate"]
+    )
 
     combined_periods = list(roundrobin(talking_periods, non_talking_periods))
 
-    for (start,end,speaker) in combined_periods:
+    for start, end, speaker in combined_periods:
         if speaker == "None" and start >= end:
-            combined_periods.remove((start,end,speaker))
+            combined_periods.remove((start, end, speaker))
 
     if combined_periods[-1][1] < audio_duration:
         combined_periods.append((combined_periods[-1][1], audio_duration, "None"))
 
-    sample['timestamps_start'] = [start for start, _, _ in combined_periods]
-    sample['timestamps_end'] = [end for _, end, _ in combined_periods]
-    sample['speakers'] = [speaker for _, _, speaker in combined_periods]
-    return sample
+    new_sample["timestamps_start"] = [start for start, _, _ in combined_periods]
+    new_sample["timestamps_end"] = [end for _, end, _ in combined_periods]
+    new_sample["speakers"] = [speaker for _, _, speaker in combined_periods]
+    return new_sample
