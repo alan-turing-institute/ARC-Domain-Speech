5436018aae150042c58d49cb72f1213848587868
Alex Hickey <alexander.john.hickey@gmail.com>
2025-12-22 12:20:50 +0000

    :coat: Segment analysis script finds optimum

--- scripts/segment_analysis.py
+++ scripts/segment_analysis.py
@@ -15,16 +15,77 @@ from dr_sad.utils import get_experiment_name
 MAIN_DIR = Path(__file__).resolve().parent.parent
 
 TOLERANCE_SECONDS = 0.25
-# Evaluation parameters
+# Starting evaluation parameters
 SPEECH_THRESHOLD = 0.5
 GAP_THRESHOLD = 0.2
 MIN_DURATION_OFF = 0.2
 MIN_DURATION_ON = 0.3
 
 
+def get_best_parameters(
+    validation_data_path: Path,
+    data_name: str,
+) -> dict[str, float | None]:
+    """Get the best parameters for the given validation data.
+
+    Args:
+        validation_data_path (Path): Path to the validation data file.
+        data_name (str): Name of the dataset used.
+
+    Returns:
+        best_params (dict): Dictionary containing the best parameter values.
+    """
+
+    data_dir = MAIN_DIR / "data" / data_name
+
+    model_metadata = yaml.safe_load(
+        (validation_data_path.parent.parent / "model_metadata.yaml").read_text()
+    )
+    time_start = model_metadata["frame_center_start"] / model_metadata["sample_rate"]
+    time_step = model_metadata["frame_center_step"] / model_metadata["sample_rate"]
+
+    # Load predictions and convert to numpy arrays
+    st_predictions = load_file(validation_data_path)
+    predictions = {
+        file_id: pred_tensor.flatten().numpy()
+        for file_id, pred_tensor in st_predictions.items()
+    }
+
+    references = {}
+    for file_id in predictions:
+        speech_segments = load_annotations(file_id, data_dir)
+        references[file_id] = speech_segments
+
+    # Evaluate predictions
+    seg_evaluator = SegmentEvaluator(
+        prediction_set=predictions,
+        reference_set=references,
+        time_start=time_start,
+        time_step=time_step,
+        tolerance=TOLERANCE_SECONDS,
+        speech_threshold=SPEECH_THRESHOLD,
+        gap_threshold=GAP_THRESHOLD,
+        min_duration_off=MIN_DURATION_OFF,
+        min_duration_on=MIN_DURATION_ON,
+    )
+
+    seg_evaluator.optimise_diff_evol(
+        speech_threshold=True,
+        gap_threshold=True,
+        min_duration_off=True,
+        min_duration_on=True,
+    )
+
+    return seg_evaluator.get_parameters()
+
+
 def evaluate_set(
     prediction_data_path: Path,
     data_name: str,
+    speech_threshold: float = SPEECH_THRESHOLD,
+    gap_threshold: float = GAP_THRESHOLD,
+    min_duration_off: float = MIN_DURATION_OFF,
+    min_duration_on: float = MIN_DURATION_ON,
 ) -> tuple[str, float]:
     """Evaluate the data in the given path.
 
@@ -67,10 +128,10 @@ def evaluate_set(
         time_start=time_start,
         time_step=time_step,
         tolerance=TOLERANCE_SECONDS,
-        speech_threshold=SPEECH_THRESHOLD,
-        gap_threshold=GAP_THRESHOLD,
-        min_duration_off=MIN_DURATION_OFF,
-        min_duration_on=MIN_DURATION_ON,
+        speech_threshold=speech_threshold,
+        gap_threshold=gap_threshold,
+        min_duration_off=min_duration_off,
+        min_duration_on=min_duration_on,
     )
 
     f1_score = seg_evaluator.f1_score()
@@ -91,6 +152,13 @@ def main(experiment_config_path: str, exclude_domain: int | None):
         data_cfg = yaml.safe_load(f)
     data_name = data_cfg["name"]
 
+    if data_cfg.get("domain_type") == "exclude_one" and exclude_domain is None:
+        msg = (
+            "Error: When data domain_type is 'exclude_one', "
+            "--exclude-domain argument must be provided."
+        )
+        raise ValueError(msg)
+
     output_path = (
         MAIN_DIR / "outputs" / experiment_path.stem
         if exclude_domain is None
@@ -100,16 +168,24 @@ def main(experiment_config_path: str, exclude_domain: int | None):
 
     f1_scores = {}
 
+    optimsed_params = get_best_parameters(
+        output_path / "saved_predictions" / "validation.safetensors",
+        data_name=data_name,
+    )
+
     for prediction_path in predictions_paths:
         set_name, set_f1 = evaluate_set(
             prediction_data_path=prediction_path,
             data_name=data_name,
+            **optimsed_params,  # type: ignore[arg-type]
         )
         f1_scores[set_name] = set_f1
 
+    analysis_results = {"f1_scores": f1_scores, "optimised_parameters": optimsed_params}
+
     output_file_path = output_path / "segment_analysis.yaml"
     with open(output_file_path, "w") as file:
-        yaml.dump(f1_scores, file)
+        yaml.dump(analysis_results, file)
 
 
 if __name__ == "__main__":
