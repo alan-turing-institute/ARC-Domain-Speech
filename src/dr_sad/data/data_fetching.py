f4cd2370a75ff446fb085527d16411c8cdbef0ef
Jack Dymond <ziph8153@bask-pg-login01.cluster.baskerville.ac.uk>
2026-02-20 16:25:14 +0000

    minor changes for new dataset, and added data config files

--- src/dr_sad/data/data_fetching.py
+++ src/dr_sad/data/data_fetching.py
@@ -51,6 +51,17 @@ DOMAIN_SETTINGS: dict[str, DomainSetting] = {
             "clinical": 4,
         },
     },
+    "dihard_synthetic": {
+        "file_name": "dihard_synthetic",
+        "domain_column": "domain",
+        "domains_idx": {
+            "webvideo": 0,
+            "restaurant": 1,
+            "meeting": 2,
+            "socio_field": 3,
+            "clinical": 4,
+        },
+    },
     "test": {
         "file_name": "test_dataset",
         "domain_column": "domain",
